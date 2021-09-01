d1ef3754b8df0b96131211cd399e3e27ed253615
Daniel Mohr <daniel.mohr@dlr.de>
2021-09-01 16:14:06 +0200

    d6

--- src/pfu_module/scripts/create_subparser_check_checksum.py
+++ src/pfu_module/scripts/create_subparser_check_checksum.py
@@ -70,6 +70,7 @@ def create_subparser_check_checksum(subparsers):
         'All hash files and data files have to be in this directory tree or ' +
         'these directory trees.',
         metavar='dir')
+    # pep8: disable=W605
     parser.add_argument(
         '-hash_extension',
         nargs="+",
